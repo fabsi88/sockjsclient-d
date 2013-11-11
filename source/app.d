a6251ff6fedfc8a514446285d455c715baac37ab
Fabian Wallentowitz <Fabian@wallentowitz.de>
2013-11-11 21:18:22 +0100

    commit1

--- source/app.d
+++ source/app.d
@@ -1,6 +1,18 @@
 import std.stdio;
+import vibe.d;
+import sockjsclient.client;
 
-void main()
-{ 
-	writeln("Edit source/app.d to start your project.");
+
+
+shared static this()
+{
+	requestHTTP("http://localhost:8989/test",
+				(scope req) {
+					
+				},
+				(scope res) {
+					logInfo("Response: %s", res.bodyReader.readAllUTF8());
+				}
+				);
+
 }
